e478c5f903d13b3d5fab2ae8a982d4c31c97e361
Michael Park <tjp4ca@gmail.com>
2022-10-12 22:34:16 -0400

    add heroku

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mern-book-search
+- converted a restful api to a graphql api
+- it lets users to search and save books
+
+heroku: https://mern-book-search-save.herokuapp.com/
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,8 +17,6 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        
-        ???
         saveBook(book: saveBookInput): User
         removeBook(bookId: ID!): User
     }
